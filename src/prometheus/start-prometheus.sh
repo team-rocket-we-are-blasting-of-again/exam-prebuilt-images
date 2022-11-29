33416b7ad2c75fa48a1b7b223eee3fc4fe28783a
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-11-29 20:33:55 +0100

    feat: make prometheus listen to data from all environments

--- src/prometheus/start-prometheus.sh
+++ src/prometheus/start-prometheus.sh
@@ -2,8 +2,26 @@
 
 sed -i "s/{{USERNAME}}/${USERNAME}/g" /etc/prometheus/prometheus.yml
 sed -i "s/{{PASSWORD}}/${PASSWORD}/g" /etc/prometheus/prometheus.yml
-the_hosts=${PROMETHEUS_HOSTS//,/\",\"}
-sed -i "s/{{PROMETHEUS_HOSTS}}/${the_hosts}/g" /etc/prometheus/prometheus.yml
+
+the_hosts=""
+services=$(echo $PROMETHEUS_HOSTS | tr "," "\n")
+for service in $services ; do
+    echo $service
+done
+
+the_hosts=""
+services=$(echo $PROMETHEUS_HOSTS | tr "," "\n")
+for service in $services ; do
+    name=$(echo $service | cut -d':' -f 1)
+    port=$(echo $service | cut -d':' -f 2)
+    the_hosts="$the_hosts\"$name.production.svc.cluster.local:$port\","
+    the_hosts="$the_hosts\"$name.staging.svc.cluster.local:$port\","
+    the_hosts="$the_hosts\"$name.test.svc.cluster.local:$port\","
+done
+
+# Remove the last comma
+the_hosts=`echo $the_hosts | sed 's/.$//'`
+sed -i "s/\"{{PROMETHEUS_HOSTS}}\"/${the_hosts}/g" /etc/prometheus/prometheus.yml
 
 /bin/prometheus \
     --config.file=/etc/prometheus/prometheus.yml \
