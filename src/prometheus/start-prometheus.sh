5f07962562d2a3086d97b2cc9b354797e0bd872f
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-12-15 23:08:22 +0100

    fix: set the scrape interval up

--- src/prometheus/start-prometheus.sh
+++ src/prometheus/start-prometheus.sh
@@ -4,17 +4,17 @@ sed -i "s/{{USERNAME}}/${USERNAME}/g" /etc/prometheus/prometheus.yml
 sed -i "s/{{PASSWORD}}/${PASSWORD}/g" /etc/prometheus/prometheus.yml
 
 the_hosts=""
-services=$(echo $PROMETHEUS_HOSTS | tr "," "\n")
+services=$(echo "$PROMETHEUS_HOSTS" | tr "," "\n")
 for service in $services ; do
-    name=$(echo $service | cut -d':' -f 1)
-    port=$(echo $service | cut -d':' -f 2)
+    name=$(echo "$service" | cut -d':' -f 1)
+    port=$(echo "$service" | cut -d':' -f 2)
     the_hosts="$the_hosts\"$name.production.svc.cluster.local:$port\","
     the_hosts="$the_hosts\"$name.staging.svc.cluster.local:$port\","
     the_hosts="$the_hosts\"$name.test.svc.cluster.local:$port\","
 done
 
 # Remove the last comma
-the_hosts=`echo $the_hosts | sed 's/.$//'`
+the_hosts=$(echo "$the_hosts" | sed 's/.$//')
 sed -i "s/\"{{PROMETHEUS_HOSTS}}\"/${the_hosts}/g" /etc/prometheus/prometheus.yml
 
 /bin/prometheus \
